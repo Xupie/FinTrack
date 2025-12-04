c7bb85a2740f366610e4c4c332238e108a035d61
xupie <example@example.com>
2025-12-04 14:52:42 +0200

    structure

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM php:8.4.15-fpm-alpine
+
+WORKDIR /var/www/html
---
+++ web/next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "./web/node_modules/next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+};
+
+export default nextConfig;
