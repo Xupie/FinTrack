212c0a155c021b42684cbed3aba2dc7fec9738fc
xupie <example@example.com>
2025-12-09 14:06:57 +0200

    add categories from api

--- web/src/app/components/transaction/newTransaction.tsx
+++ web/src/app/components/transaction/newTransaction.tsx
@@ -1,21 +1,26 @@
 import Button from "../buttons/button";
 
 type NewTransactionProps = {
+    categories: {
+        category_name: string;
+        id: number;
+        type: string;
+    }[];
     createTransaction: () => void;
     cancel: () => void;
 }
 
-export default function NewTransaction({ createTransaction, cancel }: NewTransactionProps) {
+export default function NewTransaction({ categories, createTransaction, cancel }: NewTransactionProps) {
     return (
         // Full-screen overlay
         <div className="mx-2 fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
             {/* Modal container */}
             <div className="bg-surface rounded-lg shadow-xl w-full max-w-3xl p-6 relative">
-                {/* Amount Input */}
-                <div className="flex flex-col gap-6">
-                    <div className="flex flex-col mx-auto w-3/5">
-                        <label htmlFor="amount" className="mb-1 font-medium text-foreground">
-                            Määrä
+                <div className="flex flex-col mx-auto sm:w-3/5 gap-4">
+                    {/* Amount */}
+                    <div>
+                        <label htmlFor="amount" className="flex mb-1 font-medium text-foreground">
+                            Amount
                         </label>
                         <input
                             type="text"
@@ -24,20 +29,34 @@ export default function NewTransaction({ createTransaction, cancel }: NewTransac
                             placeholder="123.45€"
                             className="border text-background bg-foreground border-gray-300 rounded-lg px-3 py-2"
                         />
+                    </div>
 
-                        <div className="flex text-foreground">
-                            <label htmlFor="meno">Meno</label>
-                            <input type="radio" name="radio_select" id="meno" />
-                            <label htmlFor="tulo">Tulo</label>
-                            <input type="radio" name="radio_select" id="tulo" />
-                        </div>
 
+                    {/* Transaction type */}
+                    <div className="flex text-foreground">
+                        <label htmlFor="expense">Expense</label>
+                        <input type="radio" name="type" id="expense" />
+                        <label htmlFor="income">Income</label>
+                        <input type="radio" name="type" id="income" />
+                    </div>
+
+                    {/* Category */}
+                    <div className="flex">
                         <select name="category" id="category">
-                            <option value="none">Ei kategoriaa</option>
+                            <option id="none" value="none">No category</option>
+                             {categories && categories.map((category, index) => (
+                                <option key={category.id} value={category.id}>{category.category_name}</option>
+                            ))}
                         </select>
+                        <div className="flex ms-auto">
+                            <Button size="sm" text="New Category" type="primary" />
+                        </div>
+                    </div>
 
-                        <label htmlFor="description" className="mb-1 font-medium text-foreground">
-                            Description
+                    {/* Description */}
+                    <div>
+                        <label htmlFor="description" className="flex mb-1 font-medium text-foreground">
+                           
                         </label>
                         <input
                             type="text"
@@ -46,26 +65,23 @@ export default function NewTransaction({ createTransaction, cancel }: NewTransac
                             placeholder="Groceries..."
                             className="border text-background bg-foreground border-gray-300 rounded-lg px-3 py-2"
                         />
-
                     </div>
+                </div>
 
-
-
-                    {/* Buttons */}
-                    <div className="flex justify-around mt-4">
-                        <Button
-                            size="xl"
-                            text="Lisää"
-                            type="primary"
-                            onClick={createTransaction}
-                        />
-                        <Button
-                            size="xl"
-                            text="Peruuta"
-                            type="cancel"
-                            onClick={cancel}
-                        />
-                    </div>
+                {/* Buttons */}
+                <div className="flex justify-around mt-4">
+                    <Button
+                        size="xl"
+                        text="Create"
+                        type="primary"
+                        onClick={createTransaction}
+                    />
+                    <Button
+                        size="xl"
+                        text="Cancel"
+                        type="cancel"
+                        onClick={cancel}
+                    />
                 </div>
             </div>
         </div>
--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -7,11 +7,32 @@ import NewTransaction from "../components/transaction/newTransaction";
 import Image from "next/image";
 import Calendar from "../components/calendar";
 
+type budgetType = {
+    income: number;
+    expense: number;
+    nettobudjetti: number;
+    transactions: {
+        id: number;
+        description: string;
+        amount: number;
+        type: string;
+        created_at: string;
+        category_name: string;
+    }[]
+}
+
+type categoryType = {
+    category_name: string;
+    id: number;
+    type: string;
+}[]
+
 export default function Dashboard() {
     const router = useRouter();
 
     const [newTransactionVisible, setNewTransactionVisible] = useState(false);
-    const [budget, setBudget] = useState([]);
+    const [budget, setBudget] = useState<budgetType | null>(null);
+    const [categories, setCategories] = useState<categoryType>([]);
 
     const onClickNewTransaction = () => {
         setNewTransactionVisible(!newTransactionVisible);
@@ -25,7 +46,7 @@ export default function Dashboard() {
                 credentials: 'include',
                 body: JSON.stringify({
                     year: date.getFullYear(),
-                    month: date.getMonth(),
+                    month: date.getMonth() + 1,
                     include_transactions: true,
                 }),
             });
@@ -39,43 +60,96 @@ export default function Dashboard() {
             }
 
             const data = await response.json();
+            setBudget(data);
         };
 
+        async function getCategories() {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/main.php?action=show_categories`, {
+                method: "GET",
+                credentials: 'include',
+            });
+
+            if (response.status === 401) {
+                router.push("/login");
+            }
+
+            if (!response.ok) {
+                console.error("Failed to fetch category data");
+            }
+
+            const data = await response.json();
+            console.log(data)
+            setCategories(data);
+        };
+        
+        getCategories();
         getDashboardData();
     }, []);
 
     async function createTransaction() {
+
+        const amount = (document.querySelector("input[name=amount]") as HTMLInputElement).value;
+        const type = (document.querySelector("input[name=type]:checked") as HTMLInputElement).id;
+        const category = (document.querySelector("select[name=category]") as HTMLSelectElement).value;
+        const description = (document.querySelector("input[name=description]") as HTMLInputElement).value;
+
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/main.php?action=add_transaction`, {
             method: "POST",
             credentials: 'include',
             body: JSON.stringify({
-                
+                amount: amount,
+                type: type,
+                category: category,
+                description: description,
             }),
         });
-        
+
+        const data = await response.json();
+        console.log(data)
+
     }
+
+    if (!budget) return <p>loading...</p>
 
     return (
         <main className="max-w-7xl mx-auto">
             {newTransactionVisible && (
-                <NewTransaction createTransaction={createTransaction} cancel={onClickNewTransaction} />
+                <NewTransaction categories={categories} createTransaction={createTransaction} cancel={onClickNewTransaction} />
             )}
-            <div className="sm:grid sm:grid-cols-2 m-4 sm:m-8 ">
+            <div className="sm:grid sm:grid-cols-2 m-4 sm:m-8">
                 <div className="sm:max-w-2/3 p-6 mx-auto">
-                    <PieChart />
+                    <PieChart data={{ ...budget }} />
                 </div>
                 <div className="bg-surface rounded-lg p-4">
                     <Calendar />
                     {budget && (
                         <div>
-                            <p>{budget}</p>
+                            <p>Tulot: {budget.income}</p>
+                            <p>Menot: {budget.expense}</p>
+                            <p>Budjetti: {budget.nettobudjetti}</p>
+                            <button
+                                type="button"
+                                onClick={onClickNewTransaction}
+                                className="relative rounded-2xl bg-primary p-2 my-4"
+                            >
+                                <Image
+                                    src="/mobile-nav/plus-white.svg"
+                                    alt="create new transaction"
+                                    width={28}
+                                    height={28}
+                                />
+                            </button>
                         </div>
+
+
                     )}
                 </div>
             </div>
 
-            <div>
-
+            <div className="m-4 sm:m-8">
+                {budget?.transactions?.map((transaction, index) => (
+                    <p key={transaction.id}>{transaction.amount}</p>
+                ))}
             </div>
 
 
